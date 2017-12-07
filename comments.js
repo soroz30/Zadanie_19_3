cd334b000d26b874616f5e49496b7479b99bfd55
soroz30 <soroz30@yahoo.pl>
2017-12-07 10:56:43 +0100

    Zadanie 19.4

---
+++ comments.js
@@ -0,0 +1,47 @@
+import {ADD_COMMENT, EDIT_COMMENT, REMOVE_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions';
+
+function comments(state = [], action) {
+    switch(action.type) {
+        case ADD_COMMENT:
+            return [{
+                        id: action.id,
+                        text: action.text,
+                        votes: 0
+                    }
+                    , ...state.comments];
+        case REMOVE_COMMENT:
+            return state.comments.filter(comment => comment.id !== action.id);
+        case EDIT_COMMENT:
+            return state.comments.map(comment => {
+                    if (comment.id == action.id) {
+                        return Object.assign({}, comment, {
+                            text: action.text
+                        });
+                    } else {
+                        return comment;
+                    }
+                });
+        case THUMB_UP_COMMENT:
+            return state.comments.map(comment => {
+                    if (comment.id === action.id) {
+                        return Object.assign({}, comment, {
+                            votes: comment.votes + 1
+                        });
+                    } else {
+                        return comment;
+                    }
+                });
+        case THUMB_DOWN_COMMENT:
+            return state.comments.map(comment => {
+                    if (comment.id === action.id) {
+                        return Object.assign({}, comment, {
+                            votes: comment.votes - 1
+                        });
+                    } else {
+                        return comment;
+                    }
+                });
+        default:
+            return state;
+    }
+}
